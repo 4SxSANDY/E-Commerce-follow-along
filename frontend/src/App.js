a1a9ae0b2609d46b30d3b08baf94135fe5122aaa
sandanam <santhanamk9604@gmail.com>
2025-02-05 12:50:54 +0530

    added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from './pages/LoginPage';  // Import directly
 import SignUpPage from './pages/SignUpPage';
 import Home from './pages/Home';
+import CreateProduct from './pages/createProduct';
 import "./App.css";
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
         <Route path='/' element={<Home/>}/>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/create-product" element={<CreateProduct/>}/>
       </Routes>
     </BrowserRouter>
   );
