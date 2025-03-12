fc2227917fe279c1ba284c135b0514c949ba2a77
sandanam <santhanamk9604@gmail.com>
2025-03-12 18:14:51 +0530

    first commit

---
+++ E-Commerce-follow-along/frontend/src/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';  // Import directly
+import  SignupPage  from './pages/SignupPage';  
+import "./App.css";
+import Home from './pages/Home';
+import CreateProduct from './pages/CreateProduct';
+import MyProducts from './pages/MyProducts';
+// import cart from './pages/cart';
+import Cart from './pages/cart';
+import ProductDetails from './pages/ProductsDetails';
+import Profile from './pages/profile';
+import CreateAddress from './pages/createAddress';
+
+
+
+const App = () => {
+  return (
+    <>
+    {/* <h1 className='text-red-400'>Hi</h1> */}
+    <BrowserRouter>
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/createproduct" element={<CreateProduct />} />
+        <Route path="/createproduct/:id" element={<CreateProduct />} />
+        <Route path="/myproducts" element={<MyProducts />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/product/:id" element={<ProductDetails />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/create-address" element={<CreateAddress />} />
+     
+      </Routes>
+    </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
